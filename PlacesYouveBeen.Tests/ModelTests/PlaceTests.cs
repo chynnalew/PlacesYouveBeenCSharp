b7d0c22b28c0c68c0489e1afd7dcb1eaa0fabdca
chynnalew <chynnalew@yahoo.com>
2021-09-29 11:55:46 -0700

    pass test to clear list
    Co-authored-by: Diego Serafico <dseraficohernandez@gmail.com>

--- PlacesYouveBeen.Tests/ModelTests/PlaceTests.cs
+++ PlacesYouveBeen.Tests/ModelTests/PlaceTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PlacesYouveBeen.Models;
+using System.Collections.Generic;
 
 namespace PlacesYouveBeen.Tests
 {
@@ -17,6 +18,22 @@ namespace PlacesYouveBeen.Tests
     {
       Place testPlace = new Place("name");
       Assert.AreEqual("name", testPlace.CityName);
+    }
+    [TestMethod]
+    public void GetAll_StoresItemInList_True()
+    {
+      Place testPlace = new Place("name");
+      List<Place> testList = Place.GetAll();
+      Assert.AreEqual(testList[0].CityName, testPlace.CityName);
+    }
+    [TestMethod]
+    public void ClearAll_ClearsItemsInList_True()
+    {
+      Place testPlace = new Place("name");
+      Place.ClearAll();
+      List<Place> testList = Place.GetAll();
+      List<Place> emptyList = new List<Place> {};
+      CollectionAssert.AreEqual(testList, emptyList);
     }
   }
 }
